604078fc86f5c20f699a6693c4d26a1221b6b47c
Илья Вознесенский <ilya.voznesensky@gmail.com>
2023-05-09 19:06:48 +0300

    Correct loaded 40 and last from prefs

--- lib/src/features/local_storage/provider/shared_pref_provider.dart
+++ lib/src/features/local_storage/provider/shared_pref_provider.dart
@@ -15,11 +15,41 @@ class SharedPreferencesProvider implements ISharedPreferencesProvider {
 
   SharedPreferencesProvider({required this.sharedPreferencesService});
 
+  // save last 40 downloaded
   @override
   Future<void> saveArticles(List<Article> articles) async {
     try {
+      List<Article> articlesToSave = [];
+      if (articles.length >= 40) {
+        articlesToSave.addAll(articles.sublist(articles.length - 40));
+      } else {
+        List<Article>? downloaded = await getArticles();
+        if (downloaded != null) {
+          if (downloaded.length + articles.length <= 40) {
+            articlesToSave.addAll(downloaded);
+            articlesToSave.addAll(articles);
+          } else {
+            // saved = 39
+            // loaded = 39
+            // need = 40-loaded = 1
+            // savedResult = saved.sublist(saved - need)
+            // result = savedResult.addAll(savedResult);
+            // result = savedResult.addAll(loaded);
+            articlesToSave.addAll(
+                downloaded.sublist(downloaded.length - (40 - articles.length)));
+            articlesToSave.addAll(articles);
+          }
+        } else {
+          if (articles.length >= 40) {
+            articlesToSave.addAll(articles.sublist(articles.length - 40));
+          } else {
+            articlesToSave.addAll(articles);
+          }
+        }
+      }
+
       String json =
-          jsonEncode(articles.map((article) => article.toJson()).toList())
+          jsonEncode(articlesToSave.map((article) => article.toJson()).toList())
               .toString();
       Logger.d('Encoded json: $json');
       await sharedPreferencesService.saveArticles(json);
@@ -28,14 +58,21 @@ class SharedPreferencesProvider implements ISharedPreferencesProvider {
     }
   }
 
+  // get saved locally articles
   @override
   Future<List<Article>?> getArticles() async {
     try {
       String data = await sharedPreferencesService.getArticles() ?? '';
       Logger.d('Loaded json: $data');
-      return (jsonDecode(data) as List<dynamic>)
-          .map((e) => Article.fromJson(e))
-          .toList();
+      if (data == '') {
+        return null;
+      } else {
+        return (jsonDecode(data) as List<dynamic>)
+            .map((e) => Article.fromJson(e))
+            .toList()
+            .reversed
+            .toList();
+      }
     } catch (e) {
       rethrow;
     }
